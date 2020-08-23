63ce6ad547a6864f822782cff6491eeea480e841
Steve Lefevre <lefevre.10@osu.edu>
2020-08-23 17:49:01 -0400

    Added script to start tmux on login

---
+++ tmux_login.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# https://unix.stackexchange.com/a/113768/394
+
+if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+  exec tmux
+fi
