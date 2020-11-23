def64127b2a7170231e15a55261513ac3f866ca9
Steve Lefevre <lefevre.10@osu.edu>
2020-11-23 13:45:31 -0500

    Added install file

---
+++ install.sh
@@ -0,0 +1,11 @@
+FILE=./dotfiles/aliases.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/cygwin-aliases.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/drush-aliases.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/functions.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/git-aliases.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/git-branch-prompt.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/git-functions.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/grep-aliases.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/start-ssh-agent.bash && test -f $FILE && source $FILE
+FILE=./dotfiles/tmux_login.sh && test -f $FILE && source $FILE
+
