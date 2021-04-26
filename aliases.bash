abee1b4210ad8d1b93d0f747df150379a9a56451
Steve Lefevre <lefevre.10@osu.edu>
2021-04-25 20:31:30 -0400

    Added bash aliases for php artisan

--- aliases.bash
+++ aliases.bash
@@ -5,4 +5,7 @@ alias please="sudo"
 alias restart="sudo shutdown -r now"
 alias reboot="sudo shutdown -r now"
 alias halt="sudo shutdown -h now"
+
+# php artisan
 alias artisan="php artisan"
+alias art="php artisan"
