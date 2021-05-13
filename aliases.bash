1c55d153a6afdc765cdb285cf5d6e37316e2e0d6
Steve Lefevre <lefevre.10@osu.edu>
2021-05-13 15:03:28 -0400

    Added alias for version

--- aliases.bash
+++ aliases.bash
@@ -5,7 +5,9 @@ alias please="sudo"
 alias restart="sudo shutdown -r now"
 alias reboot="sudo shutdown -r now"
 alias halt="sudo shutdown -h now"
+alias version="lsb_release -a"
 
 # php artisan
 alias artisan="php artisan"
 alias art="php artisan"
+
