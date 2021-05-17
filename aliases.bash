af19942e875a31368c3da54b6093cfa17ea9f489
Steve Lefevre <steven.lefevre@stratacent.com>
2021-05-17 10:09:21 -0400

    Added headers alias

--- aliases.bash
+++ aliases.bash
@@ -6,8 +6,11 @@ alias restart="sudo shutdown -r now"
 alias reboot="sudo shutdown -r now"
 alias halt="sudo shutdown -h now"
 alias version="lsb_release -a"
+alias headers="curl -IXGET"
 
 # php artisan
 alias artisan="php artisan"
 alias art="php artisan"
+
+
 
